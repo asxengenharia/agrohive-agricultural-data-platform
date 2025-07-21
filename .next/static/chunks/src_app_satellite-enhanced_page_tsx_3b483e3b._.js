83083c26974901c03a8f68a8af4dfd8345bb62f0
asxengenharia <adrianoximendes15@gmail.com>
2025-07-21 14:34:22 -0300

    Add .next/static/chunks/src_app_satellite-enhanced_page_tsx_3b483e3b._.js 1753119261783

---
+++ .next/static/chunks/src_app_satellite-enhanced_page_tsx_3b483e3b._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_c3631f57._.js",
+  "static/chunks/node_modules_be67cac6._.js"
+],
+    source: "dynamic"
+});
